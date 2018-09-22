78c553357b90f0f55a25d12e5e288d62228f35ad
JP Cimalando <jpcima@users.noreply.github.com>
2018-09-22 14:40:26 +0200

    highlight piano keys pressed by keyboard

--- src/audio.cpp
+++ src/audio.cpp
@@ -229,6 +229,7 @@ void BankEditor::pianoKeyPress(QKeyEvent *event)
             m_generator->changeNote(note);
             m_generator->ctl_playNote();
             pianoKeyCodes.insert(key);
+            ui->piano->setNoteHighlighted(note, true);
         }
     }
     else
@@ -257,6 +258,7 @@ void BankEditor::pianoKeyRelease(QKeyEvent *event)
             m_generator->changeNote(note);
             m_generator->ctl_stopNote();
             pianoKeyCodes.remove(key);
+            ui->piano->setNoteHighlighted(note, false);
         }
     }
     else
