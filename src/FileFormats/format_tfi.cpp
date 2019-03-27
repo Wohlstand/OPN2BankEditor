496a615b87b72633000a4b3c32a35c5eaf03c4a6
Vitaly Novichkov <Wohlstand@users.noreply.github.com>
2019-03-27 19:14:58 +0300

    Fix the TFI detune map
    By mistake, the order of negative values was inverted.
    
    The discussion: https://github.com/rerrahkr/BambooTracker/issues/88

--- src/FileFormats/format_tfi.cpp
+++ src/FileFormats/format_tfi.cpp
@@ -59,12 +59,12 @@ bool TFI_MM::detectInst(const QString &filePath, char * /*magic*/)
 
 static uint8_t opn1_to_opn2[7]
 {
-    5, 6, 7, 0, 1, 2, 3
+    7, 6, 5, 0, 1, 2, 3
 };
 
 static uint8_t opn2_to_opn1[8]
 {
-    3, 4, 5, 6, 3, 0, 1, 2
+    3, 4, 5, 6, 3, 2, 1, 0
 };
 
 static uint8_t detuneToOPN2(uint8_t d)
